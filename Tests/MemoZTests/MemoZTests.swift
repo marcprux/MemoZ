aeff9571768e23231d1d081acdc6189ec1a8a758
marcprux <marc@glimpse.io>
2021-03-30 11:30:46 -0400

    test: minimum appos versions

--- Tests/MemoZTests/MemoZTests.swift
+++ Tests/MemoZTests/MemoZTests.swift
@@ -436,8 +436,8 @@ extension MemoZTests {
 #endif // !os(watchOS)
 
 
+@available(OSX 10.12, iOS 13, tvOS 13, watchOS 2, *)
 extension MemoZTests {
-    @available(OSX 10.12, iOS 13, tvOS 13, watchOS 2, *)
     func testFibZ() {
         XCTAssertEqual(55, 10.fib)
         XCTAssertEqual(55, 10.fibZ)
