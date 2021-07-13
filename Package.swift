02ad663e43af7c88d5eef6d7dc4b38a7a4e41914
marcprux <marc@glimpse.io>
2021-07-13 09:23:43 -0400

    chore: checkpoint

--- Package.swift
+++ Package.swift
@@ -3,17 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "MemoZ",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v7)
+    ],
     products: [
-        .library(
-            name: "MemoZ",
-            targets: ["MemoZ"]),
+        .library(name: "MemoZ", targets: ["MemoZ"]),
     ],
     targets: [
-        .target(
-            name: "MemoZ",
-            dependencies: []),
-        .testTarget(
-            name: "MemoZTests",
-            dependencies: ["MemoZ"]),
+        .target(name: "MemoZ", dependencies: []),
+        .testTarget(name: "MemoZTests", dependencies: ["MemoZ"]),
     ]
 )
